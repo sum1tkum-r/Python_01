e7aefdba2f7cd52c0c5728c2982955c01703f003
sum1tkum-r <kumarsumit42707@gmail.com>
2026-04-15 21:27:41 +0530

    variables_2 added

---
+++ variables_2.py
@@ -0,0 +1,10 @@
+# Type your code below
+flag = False
+boolean = True
+
+# Don't change the line below
+print(f'flag = {flag}')   
+print(f'boolean = {boolean}')   
+
+# Boolean values can be either True or False.
+# They are often used to keep trace of conditions in in a program.
