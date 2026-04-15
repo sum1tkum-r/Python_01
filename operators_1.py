70285dbce99c30701dc0068cca0203138c5f1eca
sum1tkum-r <kumarsumit42707@gmail.com>
2026-04-15 21:30:32 +0530

    operators_1 added

---
+++ operators_1.py
@@ -0,0 +1,11 @@
+# Arithmetic Operators
+# Type your code below
+a = 10
+b = 5
+
+print(f'{a} + {b} = {a + b}')
+print(f'{a} - {b} = {a - b}')
+print(f'{a} * {b} = {a * b}')
+print(f'{a} / {b} = {a / b}')
+print(f'{a} % {b} = {a % b}')
+print(f'{a} ** {b} = {a ** b}')
