041876dd12405ba61292c8005502ec66acd1ff33
sum1tkum-r <kumarsumit42707@gmail.com>
2026-04-16 14:00:51 +0530

    operators_3 modified

--- operators_3.py
+++ operators_3.py
@@ -8,3 +8,13 @@ c = b * a
 print(f'c = {c}')
 
 # The answer to the question is 24, because 8 multiplied by 3 equals 24.
+
+# Comparison Operators
+# Type your code below
+x = 15
+y = 10
+
+z = x <= y
+
+# Don't change the line below
+print(f"x = {x}, y = {y}, z = {z}")
