9d47afce05f3cadaad98f4a0d031c1594c75eb66
sum1tkum-r <kumarsumit42707@gmail.com>
2026-04-16 13:53:19 +0530

    operators_3 added

---
+++ operators_3.py
@@ -0,0 +1,10 @@
+# Type your code below
+# Replace question marks with correct values
+a = 3
+b = 8
+
+# Don't change the line below
+c = b * a
+print(f'c = {c}')
+
+# The answer to the question is 24, because 8 multiplied by 3 equals 24.
