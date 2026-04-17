2e436ef20b604860d262c68fe724322ddc8e5338
sum1tkum-r <kumarsumit42707@gmail.com>
2026-04-17 18:45:21 +0530

    operators2_1 again

--- operators2_1.py
+++ operators2_1.py
@@ -42,3 +42,13 @@ result = (a or b) and not c
 
 # Don't change the line below
 print(f"result = {result}")
+
+# Question 4:
+
+b1 = True
+b2 = True
+b3 = False
+
+# Don't change the lines below
+b4 = b1 and b2 and (not b3)
+print(f"b4 = {b4}")
