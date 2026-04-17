bf75f2395c2711b478aa8c5bc71ecfb761654b54
sum1tkum-r <kumarsumit42707@gmail.com>
2026-04-17 18:36:34 +0530

    operators2_1 added

---
+++ operators2_1.py
@@ -0,0 +1,19 @@
+# Logical Operators Part 1
+
+print(True and True)   # True
+print(True and False)  # False
+print(False and False) # False
+print(True or True)    # True
+print(True or False)   # True
+print(False or False)  # False
+print(not True)        # False
+print(not False)       # True
+
+# Question:
+age = 20
+has_license = True
+
+result = age >= 18 and has_license
+
+# Don't change the line below
+print("Eligible to drive:", result)
