b6c29972e3f74be29a0c8ebb6ae8a44f109dd771
sum1tkum-r <kumarsumit42707@gmail.com>
2026-04-18 21:26:32 +0530

    modified

--- logical_operaors_ques.py
+++ logical_operaors_ques.py
@@ -13,3 +13,20 @@ cannot_go_outside = not is_sunny or temperature < 10 or wind_speed > 30
 print("Can go hiking:", can_go_hiking)
 print("Can go swimming:", can_go_swimming)
 print("Cannot go outside:", cannot_go_outside)
+
+# logical Operators Question 1:
+
+# Initialize variables
+has_license = True
+has_space = True
+has_experience = False
+
+# Calculate conditions
+can_sell_regular_pet = (has_license or has_experience) and has_space
+can_sell_exotic_pet = (has_experience or has_experience) and has_space 
+cannot_sell_any_pet = (not has_license or not has_experience) and not has_space
+
+# Don't delete the lines below
+print("Can sell regular pet:", can_sell_regular_pet)
+print("Can sell exotic pet:", can_sell_exotic_pet)
+print("Cannot sell any pet:", cannot_sell_any_pet)
