05c128778e7225754b2e5ff2edcd2fad4b028ac3
sum1tkum-r <kumarsumit42707@gmail.com>
2026-04-18 21:27:28 +0530

    again modified

--- logical_operaors_ques.py
+++ logical_operaors_ques.py
@@ -30,3 +30,20 @@ cannot_sell_any_pet = (not has_license or not has_experience) and not has_space
 print("Can sell regular pet:", can_sell_regular_pet)
 print("Can sell exotic pet:", can_sell_exotic_pet)
 print("Cannot sell any pet:", cannot_sell_any_pet)
+
+# logical Operators Question 2:
+
+# Initialize variables
+has_license = True
+has_space = True
+has_experience = False
+
+# Calculate conditions
+can_sell_regular_pet = (has_license or has_experience) and has_space
+can_sell_exotic_pet = (has_experience or has_experience) and has_space 
+cannot_sell_any_pet = (not has_license or not has_experience) and not has_space
+
+# Don't delete the lines below
+print("Can sell regular pet:", can_sell_regular_pet)
+print("Can sell exotic pet:", can_sell_exotic_pet)
+print("Cannot sell any pet:", cannot_sell_any_pet)
