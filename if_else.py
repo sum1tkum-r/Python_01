c8b899f70ca3d64f9899c052682a6704e7325801
sum1tkum-r <kumarsumit42707@gmail.com>
2026-04-19 12:03:32 +0530

    q2 added

--- if_else.py
+++ if_else.py
@@ -15,3 +15,22 @@ else :
 
 # Don't change the line below
 print(f"status = {status}")
+
+# This program classifies the wind status based on the input wind speed.
+# this is a basic if-else statement example in Python.
+
+temperature = int(input()) # Don't change this line
+weather = "unset"
+# Type your code below
+if temperature < 0:
+    weather = "Freezing"
+elif temperature >= 0 and temperature <= 15:
+     weather = "Cold"
+elif temperature >= 16 and temperature <= 25:
+    weather = "Mild"
+else :
+    weather = "Hot"
+
+
+# Don't change the line below
+print(f"weather = {weather}")
