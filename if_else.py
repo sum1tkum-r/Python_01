a7d2a9cd8d9a414adf171cc40d571c34fc006284
sum1tkum-r <kumarsumit42707@gmail.com>
2026-04-19 12:50:55 +0530

    exapmle_2 added

--- if_else.py
+++ if_else.py
@@ -58,3 +58,26 @@ else:
 
 # Don't change below this line
 print(level_message)
+
+# Other example of nested if-else statement in Python.
+
+# Get age as an integer
+age = int(input())
+
+# Get parental guidance as a boolean (True/False)
+with_parent = input() == "true"
+
+# Declare a variable named message with "None"
+message = "None"
+
+# Write your nested if-else code here
+if age >= 18:
+    message = "You can watch any movie"
+else:
+    if with_parent:
+        message = "You can watch PG-13 movies"
+    else:
+        message = "You can only watch G-rated movies"
+
+# Don't change below this line
+print(message)
