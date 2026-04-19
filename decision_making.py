7acebce329bb6d4db110db310ba762b240ce801c
sum1tkum-r <kumarsumit42707@gmail.com>
2026-04-19 11:36:10 +0530

    q2 added

--- decision_making.py
+++ decision_making.py
@@ -10,3 +10,17 @@ if a >= b and not b < 10:
 
 c += 1
 print(f"c = {c}")
+
+# This is a simple decision making example in Python.
+
+# Initialize variables
+a = 8
+b = 10
+
+# Don't change below this line
+c = 0
+if a < b or b >= 10:
+    c = 2
+
+c += 1
+print(f"c = {c}")
