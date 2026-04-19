9e1d3fc3cf6205c0f418f931cc2057a81f6761d6
sum1tkum-r <kumarsumit42707@gmail.com>
2026-04-19 12:49:36 +0530

    nested added

--- if_else.py
+++ if_else.py
@@ -34,3 +34,27 @@ else :
 
 # Don't change the line below
 print(f"weather = {weather}")
+
+
+# Nested if-else statement example in Python.
+
+level = int(input()) # Don't change this line
+has_training = input() == "True" # Don't change this line
+level_message = "None" # Don't change this line
+
+# Write your code below
+if level <= 0:
+    level_message = "Invalid level"
+elif level <= 5:
+    level_message = "Basic weapons only"
+elif level <= 10:
+    if has_training:
+        level_message = "Access to advanced weapons granted"
+    else:
+        level_message = "Need weapon training first"
+else:
+    level_message = "Access to all weapons granted"
+
+
+# Don't change below this line
+print(level_message)
